494a224dbc59daa3043815872fc8e83514e97669
kouxiaoyu_i <kouxiaoyu_i@didichuxing.com>
2018-08-09 19:28:30 +0800

    k

--- train_unet.py
+++ train_unet.py
@@ -97,7 +97,7 @@ if __name__ == '__main__':
 
     parser.add_argument('--n_epoch', nargs='?', type=int, default=100,
                         help='# of the epochs')
-    parser.add_argument('--batch_size', nargs='?', type=int, default=5,
+    parser.add_argument('--batch_size', nargs='?', type=int, default=1,
                         help='Batch Size')
     parser.add_argument('--l_rate', nargs='?', type=float, default=1e-5, 
                         help='Learning Rate')
