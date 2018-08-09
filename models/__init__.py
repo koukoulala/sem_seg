3c90c4a412cf2285d6ab043df4f32b25d9400973
kouxiaoyu_i <kouxiaoyu_i@didichuxing.com>
2018-08-09 16:52:24 +0800

    k

---
+++ models/__init__.py
@@ -0,0 +1,30 @@
+from models.unet import *
+from models.unet_keras import *
+
+
+def get_model(name, n_classes=0):
+    model = _get_model_instance(name)
+
+    if name== 'unet_keras':
+        model = model(
+            start_neurons=16,
+            img_size_target=128
+        )
+    elif name== 'unet':
+        model=model(
+        start_filters=16,
+        in_channels=3
+    )
+    else:
+        model = model(n_classes=n_classes)
+
+    return model
+
+def _get_model_instance(name):
+    try:
+        return {
+            'unet': unet,
+            'unet_keras':unet_keras,
+        }[name]
+    except:
+        print('Model {} not available'.format(name))
